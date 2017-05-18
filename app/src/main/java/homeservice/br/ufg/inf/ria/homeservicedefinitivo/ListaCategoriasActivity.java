9e0d2d7a947732dbcadbeac76f3c8a4ccf4cfcb1
raphaelrezende <raphael.rezende@gmail.com>
2017-05-18 15:48:45 -0300

    mudando nomes

--- app/src/main/java/homeservice/br/ufg/inf/ria/homeservicedefinitivo/ListaCategoriasActivity.java
+++ app/src/main/java/homeservice/br/ufg/inf/ria/homeservicedefinitivo/ListaCategoriasActivity.java
@@ -1,7 +1,6 @@
 package homeservice.br.ufg.inf.ria.homeservicedefinitivo;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -13,24 +12,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+public class ListaCategoriasActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_categorias);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
