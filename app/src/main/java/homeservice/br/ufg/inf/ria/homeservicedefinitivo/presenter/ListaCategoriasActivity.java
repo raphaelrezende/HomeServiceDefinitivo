5c1b1321bca6abf0e3e89c7bfc83b5a8b8069137
raphaelrezende <raphael.rezende@gmail.com>
2017-05-22 16:58:19 -0300

    mudança do menu sanduiche

--- app/src/main/java/homeservice/br/ufg/inf/ria/homeservicedefinitivo/presenter/ListaCategoriasActivity.java
+++ app/src/main/java/homeservice/br/ufg/inf/ria/homeservicedefinitivo/presenter/ListaCategoriasActivity.java
@@ -49,24 +49,11 @@ public class ListaCategoriasActivity extends AppCompatActivity
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
@@ -76,17 +63,15 @@ public class ListaCategoriasActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_inicio) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_avisos) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_compras) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_editar) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_sair) {
 
         }
 
